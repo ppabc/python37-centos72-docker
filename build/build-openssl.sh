849c285fcf00ef278cf4837c4504f3cfd106cf4c
ppabc <ppabc@qq.com>
2019-08-22 11:25:43 +0800

    openssl
    openssl

--- build/build-openssl.sh
+++ build/build-openssl.sh
@@ -1,5 +1,5 @@
 cd /tmp && \
-curl -LO 'https://www.openssl.org/source/openssl-1.1.0h.tar.gz' -k && \
-tar -xf openssl-1.1.0h.tar.gz && \
-cd openssl-1.1.0h && \
+curl -LO 'https://www.openssl.org/source/openssl-1.1.0k.tar.gz' -k && \
+tar -xf openssl-1.1.0k.tar.gz && \
+cd openssl-1.1.0k && \
 ./config shared --prefix=/usr/local/openssl11 --openssldir=/usr/local/openssl11 && make && make install
